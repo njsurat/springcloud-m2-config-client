e0472992a77b05074cbae8fab785aeaf688456fc
Nirav Joshi <nj.surat@gmail.com>
2019-02-21 11:23:44 +1100

    Initial commit

---
+++ src/main/java/pluralsight/demo/SpringcloudM2ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package pluralsight.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudM2ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudM2ConfigClientApplication.class, args);
+	}
+
+}
